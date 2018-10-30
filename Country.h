f906cce79258ff44f28a5c3a102e1e49a73419c5
Ray Laughrey <raymonl4963@student.vvc.edu>
2018-10-30 10:53:46 -0700

    Update Country.h

--- Country.h
+++ Country.h
@@ -13,8 +13,8 @@ public:
    Country(); // Constructor
    void read(); // Mutator
    bool is_larger_than(Country b) const; // Compares the two countries and finds the larger area
-   bool is_more_populated_than(Country next) const; // Compares the two countries and finds the more popuated
-   bool is_denser_than(Country next) const; // Compares the two countries and finds the denser populated
+   bool is_more_populated_than(Country b) const; // Compares the two countries and finds the more popuated
+   bool is_denser_than(Country b) const; // Compares the two countries and finds the denser populated
    void printpop() const; // Prints the results
    void printarea() const; // Prints the results
    void printdensity() const; // Prints the results
